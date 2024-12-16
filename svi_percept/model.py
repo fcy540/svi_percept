04d5b62c62a50a1b664f4c1e8c25dd90840c977b
Matthew Danish <m.r.danish@uu.nl>
2024-12-16 23:33:00 +0100

    rewrite numpy calcs using torch w/parameters

--- svi_percept/model.py
+++ svi_percept/model.py
@@ -2,6 +2,7 @@ from transformers import PreTrainedModel, PretrainedConfig
 from sklearn.utils.extmath import softmax
 import numpy as np
 import torch
+import torch.nn as nn
 from huggingface_hub import hf_hub_download
 
 
@@ -24,45 +25,40 @@ class SVIPerceptModel(PreTrainedModel):
         self.repo_id = repo_id or config.repo_id
         self.categories = categories or config.categories
         self.k = k or config.k
-        # FIXME/future work: run the model in terms of torch rather than numpy
-        self.dummy = torch.nn.Linear(1,2) # avoids issues with torch
         # Load npz data from model repository
         npz_path = hf_hub_download(
             repo_id=self.repo_id,
             filename="data.npz",
             repo_type="model"
         )
-        self.data = np.load(npz_path)
+        npz_data = np.load(npz_path)
+        self.data = npz_data
+
+        self.matrices = nn.ParameterDict({
+            cat: nn.Parameter(
+                torch.from_numpy(npz_data[f'{cat}_vecs']).to(torch.float32),
+                requires_grad=False
+            ) for cat in self.categories
+        })
+
+        self.scores = nn.ParameterDict({
+            cat: nn.Parameter(
+                torch.from_numpy(npz_data[f'{cat}_scores']).to(torch.float32),
+                requires_grad=False
+            ) for cat in self.categories
+        })
 
     def forward(self, features, **kwargs):
         results = np.zeros((features.shape[0], len(self.categories)))
-        debug = False
-        features = features.numpy()
-        k = self.k
-        if debug: print('features.shape', features.shape)
+
+        device = features.device
         for cat_i, cat in enumerate(self.categories):
-            allvecs = self.data[f'{cat}_vecs']
-            if debug: print('allvecs.shape', allvecs.shape)
-            scores = self.data[f'{cat}_scores']
-            if debug: print('scores.shape', scores.shape)
-            # Compute cosine similiarity of vec against allvecs
-            # (both are already normalized)
-            cos_sim_table = features @ allvecs.T
-            if debug: print('cos_sim_table.shape', cos_sim_table.shape)
-            # Get sorted array indices by similiarity in descending order
-            sortinds = np.flip(np.argsort(cos_sim_table, axis=1), axis=1)
-            if debug: print('sortinds.shape', sortinds.shape)
-            # Get corresponding scores for the sorted vectors
-            kscores = scores[sortinds][:,:k]
-            if debug: print('kscores.shape', kscores.shape)
-            # Get actual sorted similiarity scores
-            ksims = cos_sim_table[np.expand_dims(np.arange(sortinds.shape[0]), axis=1), sortinds]
-            ksims = ksims[:,:k]
-            if debug: print('ksims.shape', ksims.shape)
-            # Apply normalization after exponential formula
-            ksims = softmax(10**ksims)
-            #ksims = ksims / np.sum(ksims)
-            # Weighted sum
-            kweightedscore = np.sum(kscores * ksims, axis=1)
-            results[:, cat_i] = kweightedscore
+            allvecs = self.matrices[cat].to(device)
+            scores = self.scores[cat].to(device)
+            similarities = torch.matmul(features, torch.transpose(allvecs, 0, 1))  # [batch_size, num_vectors]
+            raw_weights, indices = torch.topk(similarities, k=self.k, dim=1)  # [batch_size, self.k]
+            kweights = torch.pow(10.0, raw_weights)  # [batch_size, self.k]
+            kweights = torch.nn.functional.softmax(kweights, dim=1)  # [batch_size, self.k]
+            kscores = scores[indices]  # [batch_size, self.k]
+            results[:, cat_i] = torch.sum(kscores * kweights, dim=1)
         return {"results": results}
